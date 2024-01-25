83492e69d7f697e171656dffec19bb5d96bd9331
lightrav3n <135107438+lightrav3n@users.noreply.github.com>
2024-01-25 14:38:22 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // 2. import BrowserRouter
-import BrowserRouter from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
