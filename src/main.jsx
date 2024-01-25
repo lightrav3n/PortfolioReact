4e8771e4535522f38d01f5888df15d1742f631fc
lightrav3n <135107438+lightrav3n@users.noreply.github.com>
2024-01-25 15:20:24 +0100

    added hash router

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // 2. import BrowserRouter
-import {BrowserRouter} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // 3. connect the App to the browser's URL
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
